4078fde9431ffee80446d4a53986af2105a063cb
mnibor <marcelorobin@gmail.com>
2024-11-29 20:40:59 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
@@ -47,6 +48,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <Button>Click me</Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
